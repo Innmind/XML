fc36773541dea4bd71a27267473d3ff268205adf
Baptiste Langlade <baptouuuu@gmail.com>
2016-10-24 08:39:47 +0200

    add visitor to find parent node

---
+++ tests/Visitor/ParentNodeTest.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\XML\Visitor;
+
+use Innmind\XML\{
+    Visitor\ParentNode,
+    Reader\Reader,
+    Element\Element
+};
+
+class ParentNodeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInterface()
+    {
+        $tree = (new Reader)->read(<<<XML
+<div><div><foo /><bar /></div></div>
+XML
+        );
+        $parent = $tree
+            ->children()
+            ->get(0)
+            ->children()
+            ->get(0);
+        $node = $parent
+            ->children()
+            ->get(1);
+
+        $this->assertSame(
+            $parent,
+            (new ParentNode($node))($tree)
+        );
+    }
+
+    /**
+     * @expectedException Innmind\XML\Exception\NodeHasNoParentException
+     */
+    public function testThrowWhenNoParentFound()
+    {
+        (new ParentNode(new Element('foo')))(new Element('bar'));
+    }
+}
