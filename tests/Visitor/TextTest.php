961f66d5189887b6734b982675400ac44bd2d994
Baptiste Langlade <baptouuuu@gmail.com>
2016-10-25 08:15:20 +0200

    add visitor to extract whole text from a tree

---
+++ tests/Visitor/TextTest.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\XML\Visitor;
+
+use Innmind\XML\{
+    Visitor\Text,
+    Reader\Reader
+};
+
+class TextTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInterface()
+    {
+        $tree = (new Reader)->read(<<<XML
+<div>
+    <h1>Hey</h1>
+    <div>
+        <foo />
+        whatever
+        <bar />
+    </div>
+    42
+</div>
+XML
+        );
+
+        $this->assertSame(
+            "\n".
+            '    Hey'."\n".
+            '    '."\n".
+            '        '."\n".
+            '        whatever'."\n".
+            '        '."\n".
+            '    '."\n".
+            '    42'."\n",
+            (new Text)($tree)
+        );
+    }
+}
