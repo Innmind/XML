2211539832def0cdd26fa7695252b16b60185af3
Baptiste Langlade <baptouuuu@gmail.com>
2016-10-26 08:13:48 +0200

    add cache reader

---
+++ tests/Reader/CacheReaderTest.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Xml\Reader;
+
+use Innmind\Xml\{
+    Reader\CacheReader,
+    ReaderInterface,
+    NodeInterface
+};
+use Innmind\Filesystem\StreamInterface;
+
+class CacheReaderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            ReaderInterface::class,
+            new CacheReader(
+                $this->createMock(ReaderInterface::class)
+            )
+        );
+    }
+
+    public function testCache()
+    {
+        $stream = $this->createMock(StreamInterface::class);
+        $cache = new CacheReader(
+            $reader = $this->createMock(ReaderInterface::class)
+        );
+        $reader
+            ->method('read')
+            ->with($stream)
+            ->willReturnCallback(function($xml) {
+                return $this->createMock(NodeInterface::class);
+            });
+
+        $node = $cache->read($stream);
+        $this->assertInstanceOf(NodeInterface::class, $node);
+        $this->assertSame($node, $cache->read($stream));
+        $this->assertSame($cache, $cache->detach($stream));
+        $this->assertNotSame($node, $cache->read($stream));
+    }
+}
