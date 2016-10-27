d740f1f6c2fb86f193d85b12687de1f35e8701bb
Baptiste Langlade <baptouuuu@gmail.com>
2016-10-27 08:22:46 +0200

    split actions into methods

--- src/Translator/NodeTranslator/DocumentTranslator.php
+++ src/Translator/NodeTranslator/DocumentTranslator.php
@@ -25,41 +25,56 @@ final class DocumentTranslator implements NodeTranslatorInterface
             throw new InvalidArgumentException;
         }
 
-        list($major, $minor) = explode('.', $node->xmlVersion);
-        $type = $children = $encoding = null;
+        return new Document(
+            $this->buildVersion($node),
+            $node->doctype ? $this->buildDoctype($node->doctype) : null,
+            $node->childNodes ?
+                $this->buildChildren($node->childNodes, $translator) : null,
+            $node->encoding ? $this->buildEncoding($node->encoding) : null
+        );
+    }
 
-        if ($node->doctype) {
-            $type = new Type(
-                $node->doctype->name,
-                $node->doctype->publicId,
-                $node->doctype->systemId
+    private function buildVersion(\DOMDocument $document): Version
+    {
+        list($major, $minor) = explode('.', $document->xmlVersion);
+
+        return new Version(
+            (int) $major,
+            (int) $minor
+        );
+    }
+
+    private function buildDoctype(\DOMDocumentType $type): Type
+    {
+        return new Type(
+            $type->name,
+            $type->publicId,
+            $type->systemId
+        );
+    }
+
+    private function buildChildren(
+        \DOMNodeList $nodes,
+        NodeTranslator $translator
+    ): Map {
+        $children = new Map('int', NodeInterface::class);
+
+        foreach ($nodes as $child) {
+            if ($child->nodeType === XML_DOCUMENT_TYPE_NODE) {
+                continue;
+            }
+
+            $children = $children->put(
+                $children->size(),
+                $translator->translate($child)
             );
         }
 
-        if ($node->childNodes) {
-            $children = new Map('int', NodeInterface::class);
+        return $children;
+    }
 
-            foreach ($node->childNodes as $child) {
-                if ($child->nodeType === XML_DOCUMENT_TYPE_NODE) {
-                    continue;
-                }
-
-                $children = $children->put(
-                    $children->size(),
-                    $translator->translate($child)
-                );
-            }
-        }
-
-        if ($node->encoding) {
-            $encoding = new Encoding($node->encoding);
-        }
-
-        return new Document(
-            new Version((int) $major, (int) $minor),
-            $type,
-            $children,
-            $encoding
-        );
+    private function buildEncoding(string $encoding): Encoding
+    {
+        return new Encoding($encoding);
     }
 }
--- src/Translator/NodeTranslator/ElementTranslator.php
+++ src/Translator/NodeTranslator/ElementTranslator.php
@@ -25,43 +25,58 @@ final class ElementTranslator implements NodeTranslatorInterface
             throw new InvalidArgumentException;
         }
 
-        $attributes = new Map('string', AttributeInterface::class);
+        $attributes = $node->attributes ?
+            $this->buildAttributes($node->attributes) : null;
 
-        if ($node->attributes instanceof \DOMNamedNodeMap) {
-            foreach ($node->attributes as $name => $attribute) {
-                $attributes = $attributes->put(
-                    $name,
-                    new Attribute(
-                        $name,
-                        $attribute->childNodes->length === 1 ?
-                            $attribute->childNodes->item(0)->nodeValue : ''
-                    )
-                );
-            }
-        }
-
-        if ($node->childNodes->length === 0) {
+        if (
+            $node->childNodes instanceof \DOMNodeList &&
+            $node->childNodes->length === 0
+        ) {
             return new SelfClosingElement(
                 $node->nodeName,
                 $attributes
             );
         }
 
-        if ($node->childNodes) {
-            $children = new Map('int', NodeInterface::class);
-
-            foreach ($node->childNodes as $child) {
-                $children = $children->put(
-                    $children->size(),
-                    $translator->translate($child)
-                );
-            }
-        }
-
         return new Element(
             $node->nodeName,
             $attributes,
-            $children ?? null
+            $node->childNodes ?
+                $this->buildChildren($node->childNodes, $translator) : null
         );
+    }
+
+    private function buildAttributes(\DOMNamedNodeMap $map): Map
+    {
+        $attributes = new Map('string', AttributeInterface::class);
+
+        foreach ($map as $name => $attribute) {
+            $attributes = $attributes->put(
+                $name,
+                new Attribute(
+                    $name,
+                    $attribute->childNodes->length === 1 ?
+                        $attribute->childNodes->item(0)->nodeValue : ''
+                )
+            );
+        }
+
+        return $attributes;
+    }
+
+    private function buildChildren(
+        \DOMNodeList $nodes,
+        NodeTranslator $translator
+    ): Map {
+        $children = new Map('int', NodeInterface::class);
+
+        foreach ($nodes as $child) {
+            $children = $children->put(
+                $children->size(),
+                $translator->translate($child)
+            );
+        }
+
+        return $children;
     }
 }
