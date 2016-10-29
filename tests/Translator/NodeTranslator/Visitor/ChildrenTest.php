5d526deca8ed264e53efe07d5e7a2bfbe4c55a23
Baptiste Langlade <baptouuuu@gmail.com>
2016-10-29 15:25:41 +0200

    add visitor to extract children out of a node

---
+++ tests/Translator/NodeTranslator/Visitor/ChildrenTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Xml\Translator\NodeTranslator\Visitor;
+
+use Innmind\Xml\{
+    Translator\NodeTranslator\Visitor\Children,
+    Translator\NodeTranslator,
+    Translator\NodeTranslators,
+    NodeInterface
+};
+use Innmind\Immutable\MapInterface;
+
+class ChildrenTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNoChildren()
+    {
+        $document = new \DOMDocument;
+        $document->loadXML('<root></root>');
+
+        $children = (new Children(
+            new NodeTranslator(
+                NodeTranslators::defaults()
+            )
+        ))($document->childNodes->item(0));
+
+        $this->assertInstanceOf(MapInterface::class, $children);
+        $this->assertSame('int', (string) $children->keyType());
+        $this->assertSame(NodeInterface::class, (string) $children->valueType());
+        $this->assertCount(0, $children);
+    }
+
+    public function testChildren()
+    {
+        $document = new \DOMDocument;
+        $document->loadXML('<root><foo/><bar/></root>');
+
+        $children = (new Children(
+            new NodeTranslator(
+                NodeTranslators::defaults()
+            )
+        ))($document->childNodes->item(0));
+
+        $this->assertInstanceOf(MapInterface::class, $children);
+        $this->assertSame('int', (string) $children->keyType());
+        $this->assertSame(NodeInterface::class, (string) $children->valueType());
+        $this->assertCount(2, $children);
+    }
+}
