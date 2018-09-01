c84cf205b6db91f150d66a19aa2852794b95d64c
Baptiste Langlade <baptouuuu@gmail.com>
2018-09-01 15:44:09 +0200

    add a bootstrap function as an alternative to the service container

---
+++ src/bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Xml;
+
+use Innmind\Xml\{
+    ReaderInterface,
+    Reader\Reader,
+    Reader\CacheReader,
+    Translator\NodeTranslator,
+    Translator\NodeTranslators,
+};
+
+function bootstrap(): array {
+    return [
+        'reader' => new Reader(
+            new NodeTranslator(
+                NodeTranslators::defaults()
+            )
+        ),
+        'cache' => static function(ReaderInterface $reader): ReaderInterface {
+            return new CacheReader($reader);
+        },
+    ];
+}
---
+++ tests/BootstrapTest.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Xml;
+
+use function Innmind\Xml\bootstrap;
+use Innmind\Xml\{
+    Reader\Reader,
+    Reader\CacheReader,
+};
+use PHPUnit\Framework\TestCase;
+
+class BootstrapTest extends TestCase
+{
+    public function testBootstrap()
+    {
+        $services = bootstrap();
+        $reader = $services['reader'];
+        $cache = $services['cache'];
+
+        $this->assertInstanceOf(Reader::class, $reader);
+        $this->assertInternalType('callable', $cache);
+        $this->assertInstanceOf(CacheReader::class, $cache($reader));
+    }
+}
