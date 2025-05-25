65fd89d218de283527ec1bb00aac40487181d235
Baptiste Langlade <baptiste.langlade@hey.com>
2025-05-25 11:33:53 +0200

    add support for php 8.4 \Dom\* api

--- src/Translator.php
+++ src/Translator.php
@@ -26,9 +26,11 @@ final class Translator
     }
 
     /**
+     * @psalm-suppress UndefinedClass Since the package still supports PHP 8.2
+     *
      * @return Maybe<Document|Node|Element>
      */
-    public function __invoke(\DOMNode $node): Maybe
+    public function __invoke(\DOMNode|\Dom\Node $node): Maybe
     {
         return $this
             ->buildDocument($node)
@@ -44,34 +46,73 @@ final class Translator
     }
 
     /**
+     * @psalm-suppress UndefinedClass Since the package still supports PHP 8.2
+     * @psalm-suppress TypeDoesNotContainType
+     *
      * @return Maybe<Node|Element>
      */
-    private function child(\DOMNode $node): Maybe
+    private function child(\DOMNode|\Dom\Node $node): Maybe
     {
-        if ($node->nodeType === \XML_COMMENT_NODE && $node instanceof \DOMComment) {
+        if (
+            $node->nodeType === \XML_COMMENT_NODE &&
+            (
+                $node instanceof \DOMComment ||
+                $node instanceof \Dom\Comment
+            )
+        ) {
             return Maybe::just(Node::comment($node->data));
         }
 
-        if ($node->nodeType === \XML_TEXT_NODE && $node instanceof \DOMText) {
+        if (
+            $node->nodeType === \XML_TEXT_NODE &&
+            (
+                $node instanceof \DOMText ||
+                $node instanceof \Dom\Text
+            )
+        ) {
             return Maybe::just(Node::text($node->data));
         }
 
-        if ($node->nodeType === \XML_CDATA_SECTION_NODE && $node instanceof \DOMCharacterData) {
+        if (
+            $node->nodeType === \XML_CDATA_SECTION_NODE &&
+            (
+                $node instanceof \DOMCharacterData ||
+                $node instanceof \Dom\CharacterData
+            )
+        ) {
             return Maybe::just(Node::characterData($node->data));
         }
 
-        if ($node->nodeType === \XML_ENTITY_REF_NODE && $node instanceof \DOMEntityReference) {
+        if (
+            $node->nodeType === \XML_ENTITY_REF_NODE &&
+            (
+                $node instanceof \DOMEntityReference ||
+                $node instanceof \Dom\EntityReference
+            )
+        ) {
             return Maybe::just(Node::entityReference($node->nodeName));
         }
 
-        if ($node->nodeType === \XML_PI_NODE && $node instanceof \DOMProcessingInstruction) {
+        if (
+            $node->nodeType === \XML_PI_NODE &&
+            (
+                $node instanceof \DOMProcessingInstruction ||
+                $node instanceof \Dom\ProcessingInstruction
+            )
+        ) {
             return Maybe::just(Node::processingInstruction(
                 $node->nodeName,
                 $node->data,
             ));
         }
 
-        if ($node->nodeType === \XML_ELEMENT_NODE && $node instanceof \DOMElement) {
+        if (
+            $node->nodeType === \XML_ELEMENT_NODE &&
+            (
+                $node instanceof \DOMElement ||
+                $node instanceof \Dom\Element
+            )
+        ) {
             /**
              * @psalm-suppress ImpureFunctionCall
              * @psalm-suppress ImpureMethodCall
@@ -81,7 +122,11 @@ final class Translator
                 self::attributes($node),
                 $this->children(
                     Sequence::of(...\array_values(\iterator_to_array($node->childNodes)))
-                        ->keep(Instance::of(\DOMNode::class)),
+                        ->keep(
+                            Instance::of(\DOMNode::class)->or(
+                                Instance::of(\Dom\Node::class),
+                            ),
+                        ),
                 ),
             )->map(match ($node->childNodes->length) {
                 0 => Element::selfClosing(...),
@@ -94,25 +139,35 @@ final class Translator
     }
 
     /**
+     * @psalm-suppress UndefinedClass Since the package still supports PHP 8.2
+     *
      * @return Maybe<Document>
      */
-    private function buildDocument(\DOMNode $node): Maybe
+    private function buildDocument(\DOMNode|\Dom\Node $node): Maybe
     {
         /** @psalm-suppress MixedArgumentTypeCoercion */
         return Maybe::just($node)
-            ->keep(Instance::of(\DOMDocument::class))
+            ->keep(
+                Instance::of(\DOMDocument::class)->or(
+                    Instance::of(\Dom\Document::class),
+                ),
+            )
             ->flatMap(
-                fn(\DOMDocument $node) => Maybe::all(
-                    self::buildVersion($node),
+                fn($document) => Maybe::all(
+                    self::buildVersion($document),
                     $this->children(
-                        Sequence::of(...\array_values(\iterator_to_array($node->childNodes)))
-                            ->keep(Instance::of(\DOMNode::class))
+                        Sequence::of(...\array_values(\iterator_to_array($document->childNodes)))
+                            ->keep(
+                                Instance::of(\DOMNode::class)->or(
+                                    Instance::of(\Dom\Node::class),
+                                ),
+                            )
                             ->exclude(static fn($child) => $child->nodeType === \XML_DOCUMENT_TYPE_NODE),
                     ),
                 )->map(static fn(Version $version, Sequence $children) => Document::of(
                     $version,
-                    Maybe::of($node->doctype)->flatMap(self::buildDoctype(...)),
-                    Maybe::of($node->encoding)->flatMap(Encoding::maybe(...)),
+                    Maybe::of($document->doctype)->flatMap(self::buildDoctype(...)),
+                    Maybe::of($document->encoding)->flatMap(Encoding::maybe(...)),
                     $children,
                 )),
             );
@@ -121,10 +176,11 @@ final class Translator
     /**
      * @psalm-pure
      * @psalm-suppress ImpurePropertyFetch
+     * @psalm-suppress UndefinedClass Since the package still supports PHP 8.2
      *
      * @return Maybe<Version>
      */
-    private static function buildVersion(\DOMDocument $document): Maybe
+    private static function buildVersion(\DOMDocument|\Dom\Document $document): Maybe
     {
         [$major, $minor] = \explode('.', $document->xmlVersion ?? '');
 
@@ -137,10 +193,11 @@ final class Translator
     /**
      * @psalm-pure
      * @psalm-suppress ImpurePropertyFetch
+     * @psalm-suppress UndefinedClass Since the package still supports PHP 8.2
      *
      * @return Maybe<Type>
      */
-    private static function buildDoctype(\DOMDocumentType $type): Maybe
+    private static function buildDoctype(\DOMDocumentType|\Dom\DocumentType $type): Maybe
     {
         /** @psalm-suppress MixedArgument */
         return Type::maybe(
@@ -151,24 +208,35 @@ final class Translator
     }
 
     /**
+     * @psalm-suppress UndefinedClass Since the package still supports PHP 8.2
+     * @psalm-suppress TypeDoesNotContainType
+     *
      * @return Maybe<Set<Attribute>>
      */
-    private static function attributes(\DOMElement $element): Maybe
+    private static function attributes(\DOMElement|\Dom\Element $element): Maybe
     {
         /** @var Set<Attribute> */
         $attributes = Set::of();
         $attrs = [];
 
-        if ($element->attributes instanceof \DOMNamedNodeMap) {
+        if (
+            $element->attributes instanceof \DOMNamedNodeMap ||
+            $element->attributes instanceof \Dom\NamedNodeMap
+        ) {
             /**
              * @psalm-suppress ImpureFunctionCall
              * @psalm-suppress ImpureMethodCall
+             * @psalm-suppress PossiblyInvalidArgument Due to \Dom\NamedNodeMap for PHP 8.2
              */
             $attrs = \iterator_to_array($element->attributes);
         }
 
         return Sequence::of(...\array_values($attrs))
-            ->keep(Instance::of(\DOMAttr::class))
+            ->keep(
+                Instance::of(\DOMAttr::class)->or(
+                    Instance::of(\Dom\Attr::class),
+                ),
+            )
             ->sink($attributes)
             ->maybe(
                 static fn($attributes, $attribute) => Attribute::maybe(
@@ -179,7 +247,9 @@ final class Translator
     }
 
     /**
-     * @param Sequence<\DOMNode> $children
+     * @psalm-suppress UndefinedDocblockClass Since the package still supports PHP 8.2
+     *
+     * @param Sequence<\DOMNode|\Dom\Node> $children
      *
      * @return Maybe<Sequence<Node|Element>>
      */
