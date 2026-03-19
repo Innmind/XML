f49eea7cd00ae37b2a39fbd45710d286b05092e4
Baptiste Langlade <baptiste.langlade@hey.com>
2026-03-19 20:58:33 +0100

    add Node::raw()

---
+++ src/Node/Raw.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Xml\Node;
+
+/**
+ * @internal
+ * @psalm-immutable
+ */
+final class Raw implements Implementation
+{
+    private function __construct(private string $data)
+    {
+    }
+
+    /**
+     * @psalm-pure
+     */
+    public static function of(string $data): self
+    {
+        return new self($data);
+    }
+
+    #[\Override]
+    public function content(): string
+    {
+        return $this->data;
+    }
+
+    #[\Override]
+    public function render(\XMLWriter $writer): string
+    {
+        /** @psalm-suppress ImpureMethodCall */
+        $writer->writeRaw($this->data);
+
+        /** @psalm-suppress ImpureMethodCall */
+        return $writer->outputMemory();
+    }
+}
